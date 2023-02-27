e48480a720a5fd037aa8ef39a22bc7dc1ec67fb5
Abhishek Aman <70830645+Abhishek-Aman1809@users.noreply.github.com>
2023-02-27 11:26:51 +0530

    query selectors used

---
+++ dom.js
@@ -0,0 +1,7 @@
+var header = document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+var input=document.querySelector('input');
+input.value='User Name'
+
+var item=document.querySelector('.list-group-item');
+item.style.color='red';
