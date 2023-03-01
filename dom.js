fbfd3bac6870b6a572379d44cb7fa377d749d8b9
Abhishek Aman <70830645+Abhishek-Aman1809@users.noreply.github.com>
2023-03-01 08:43:37 +0530

    list font and 3rd element background color change

--- dom.js
+++ dom.js
@@ -1,7 +1,16 @@
-var header = document.querySelector('#main-header');
-header.style.borderBottom='solid 4px #ccc';
-var input=document.querySelector('input');
-input.value='User Name'
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+// var input=document.querySelector('input');
+// input.value='User Name'
 
-var item=document.querySelector('.list-group-item');
-item.style.color='red';
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].style.backgroundColor ='green';
+
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight='bold';
+}
