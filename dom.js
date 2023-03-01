557fe1ec2eadb3ee5e5f022bf18369d81065f65e
Abhishek Aman <70830645+Abhishek-Aman1809@users.noreply.github.com>
2023-03-01 12:36:10 +0530

    Delete and Edit functionality

--- dom.js
+++ dom.js
@@ -24,16 +24,70 @@
 //     li[i].style.fontWeight='bold';
 // }
 
-var item=document.querySelector('.list-group-item');
-var secondItem = document.querySelector('.list-group-item:nth-child(3)');
-secondItem.style.color='green';
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color='white';
+// var item=document.querySelector('.list-group-item');
+// var secondItem = document.querySelector('.list-group-item:nth-child(3)');
+// secondItem.style.color='green';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='white';
 
-//query Selector Alll
+// //query Selector Alll
 
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor='green';
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor='green';
+// }
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click', removeItem);
+
+filter.addEventListener('keyup', filterItems);
+
+function addItem(e){
+  e.preventDefault();
+
+  var newItem = document.getElementById('item').value;
+
+  var li = document.createElement('li');
+
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
+
+  var deleteBtn = document.createElement('button');
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  li.appendChild(deleteBtn);
+
+  itemList.appendChild(li);
 }
 
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+function filterItems(e){
+
+  var text = e.target.value.toLowerCase();
+ 
+  var items = itemList.getElementsByTagName('li');
+
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
