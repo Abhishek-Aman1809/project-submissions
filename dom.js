f6c2c5586d04415a4186253df49a6eeeb44ff200
Abhishek Aman <70830645+Abhishek-Aman1809@users.noreply.github.com>
2023-03-01 09:20:57 +0530

    used Query selector

--- dom.js
+++ dom.js
@@ -15,13 +15,25 @@
 //     items[i].style.fontWeight='bold';
 // }
 
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[2].style.backgroundColor ='green';
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[2].style.backgroundColor ='green';
 
-for(var i=0; i<li.length; i++){
-    li[i].style.fontWeight='bold';
+// for(var i=0; i<li.length; i++){
+//     li[i].style.fontWeight='bold';
+// }
+
+var item=document.querySelector('.list-group-item');
+var secondItem = document.querySelector('.list-group-item:nth-child(3)');
+secondItem.style.color='green';
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color='white';
+
+//query Selector Alll
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor='green';
 }
-
 
